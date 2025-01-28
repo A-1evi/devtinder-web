174c1517bc4467dfd2c39af60a1d5cf1e6f1c7b5
Alankar <alankar010499@gmail.com>
2025-01-28 20:53:40 +0530

    add Redux store setup, user slice, and implement login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,60 @@
-
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router";
+import { BASE_URL } from "../utils/constants";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [email, setEmail] = useState("alankar@gmail.com");
+  const [password, setPassword] = useState("Alankar@123");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/login",
+        { email, password },
+        { withCredentials: true }
+      );
 
-export default Login
+      dispatch(addUser(res.data));
+      return navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  return (
+    <div className="card card-border bg-base-100 w-96 mx-auto my-12">
+      <div className="card-body">
+        <h2 className="card-title">Login</h2>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">What is your email?</legend>
+          <input
+            type="text"
+            className="input"
+            placeholder=""
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </fieldset>
+        <fieldset className="fieldset">
+          <legend className="fieldset-legend">Password</legend>
+          <input
+            type="text"
+            className="input"
+            placeholder=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </fieldset>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary" onClick={handleLogin}>
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
