547dc521aa9e79faded58e9801a619e29e53ec5e
Alankar Singh <alankar010499@gmail.com>
2025-06-30 13:42:08 +0530

    fix: correct BASE_URL definition to include full API endpoint for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,4 @@
-
-
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
-
- 
+  location.hostname === "localhost"
+    ? "http://localhost:3000"
+    : "https://devtinder-j9bz.onrender.com/api";
