930d1e7e39a19a9d8ba3644f296b8040f7266538
Alankar Singh <alankar010499@gmail.com>
2025-06-30 13:44:57 +0530

    fix: update BASE_URL to remove unnecessary API path for production

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:3000"
-    : "https://devtinder-j9bz.onrender.com/api";
+    : "https://devtinder-j9bz.onrender.com";
